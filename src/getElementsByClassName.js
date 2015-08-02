eeeb987576f4bb06ac1b5b4eeac7f247893f3fd0
Gary Ye <garysye@gmail.com>
2015-08-02 15:23:04 -0500

    Completed function 'getElementsByClassName'
    Created getElementsByClassName as a recursive function with two paremeters: 'className' and 'element'.  If element was null, would call function with 'document.body'.  If valid, it would go through childNodes and classLists for matching elements.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className, element) {
+	// your code here
+	var output = [];
+
+	if (element === undefined) {
+		output = output.concat(getElementsByClassName(className, document.body));
+	}
+
+	if (typeof element === 'object' && element !== null) {
+		if(element.classList !== undefined) {
+			for (var i = 0; i < element.classList.length; i++) {
+				if (element.classList[i] === className) {
+					output.push(element);
+					i = element.classList.length;
+				}
+			}
+		}
+		if(element.childNodes !== undefined) {
+			for (var i = 0; i < element.childNodes.length; i++) {
+				var elementsOut = getElementsByClassName(className, element.childNodes[i]);
+				if (elementsOut.length > 0) {
+					output = output.concat(elementsOut);
+				}
+			}
+		}
+	}
+	return output;
 };
