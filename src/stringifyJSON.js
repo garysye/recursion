15c0bb5b43811c427ee902028b73c41da82b7aee
Gary Ye <garysye@gmail.com>
2015-08-02 13:52:19 -0500

    Completed function 'stringifyJSON'
    Used a recursive method with a special loop each for arrays and object. Set special classes if the input is null, undefined, or a function.  Created a new function 'prepareNext' for following items/keys in an array or object.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var outputStr = '';
+  if (Array.isArray(obj)) {
+  	outputStr += '[';
+  	for (var i = 0; i < obj.length; i++) {
+  		if (typeof obj[i] !== 'function' && typeof obj[i] !== 'undefined') {
+	  		outputStr = prepareNext(outputStr);
+	  		outputStr += stringifyJSON(obj[i]);
+  		}
+  	}
+  	outputStr += ']';
+  	return outputStr;
+  }
+  if (obj !== null && typeof obj === 'object') {
+  	outputStr += '{';
+  	for (var key in obj) {
+  		if (typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined') {
+	  		outputStr = prepareNext(outputStr);
+	  		outputStr += ('"' + key + '":' + stringifyJSON(obj[key]));
+	  	}
+  	}
+  	outputStr += '}';
+  	return outputStr;
+  }
+  if (typeof obj === 'function') {
+  	return null;
+  }
+  if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+  return String(obj);
 };
+
+var prepareNext = function(str) {
+	var prev  = str[str.length - 1];
+	if (prev !== '[' && prev !== '{') {
+		str += ',';
+	} 
+	return str;
+}
